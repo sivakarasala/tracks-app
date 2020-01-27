7d50120b72781e0a85010962f168cf55854205d5
Siva Krishna <shivataraya@gmail.com>
2020-01-27 07:46:52 -0600

    setup mock location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,43 @@
-import React from "react";
+// import "../_mockLocation";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
 import Map from "../components/Map";
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy
+} from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+        location => {
+          //console.log(location);
+        }
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {err ? <Text>PLease enable location services</Text> : null}
     </SafeAreaView>
   );
 };
