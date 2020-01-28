7999a728ac06863cb42bad68d67f0e703fa21221
Siva Krishna <shivataraya@gmail.com>
2020-01-28 06:28:26 -0600

    wireup track listing

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Context as TrackContext } from "../context/trackContext";
 
-const TrackDetailScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>;
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam("_id");
+
+  const track = state.find(t => t._id === _id);
+
+  return <Text style={{ fontSize: 48 }}>{track.name}</Text>;
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,15 +1,32 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useContext } from "react";
+import { Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { ListItem } from "react-native-elements";
+import { Context as TrackContext } from "../context/trackContext";
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+  console.log(state);
   return (
-    <View>
+    <>
+      <NavigationEvents onWillFocus={fetchTracks} />
       <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate("TrackDetail")}
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("TrackDetail", { _id: item._id })
+              }
+            >
+              <ListItem chevron title={item.name} />
+            </TouchableOpacity>
+          );
+        }}
       />
-    </View>
+    </>
   );
 };
 
