d18c7b0528a1475d8d3b8220655809b82192673f
Siva Krishna <shivataraya@gmail.com>
2020-01-27 19:17:19 -0600

    extract logic to useLocation hook

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync
+} from "expo-location";
+
+export default callback => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10
+        },
+        location => {
+          callback(location);
+        }
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [err];
+};
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,40 +1,16 @@
 // import "../_mockLocation";
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
 import Map from "../components/Map";
-import {
-  requestPermissionsAsync,
-  watchPositionAsync,
-  Accuracy
-} from "expo-location";
 import { Context as LocationContext } from "../context/locationContext";
+import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync();
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        location => {
-          addLocation(location);
-        }
-      );
-    } catch (e) {
-      setErr(e);
-    }
-  };
+  const [err] = useLocation(addLocation);
 
-  useEffect(() => {
-    startWatching();
-  }, []);
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
